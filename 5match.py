1a47a01337a4570b6ffceb69f8e2daf5fd379ac9
iamshengfu <40295575+iamshengfu@users.noreply.github.com>
2019-02-04 21:32:41 +0800

    Update 5match.py

--- 5match.py
+++ 5match.py
@@ -5,6 +5,8 @@ four = 0
 five = 0
 raw = [[random.randint(0, 1) for x in range(5)] for y in range(1000)]
 
+
+#test how likely to win or lose three games in a row, 3:0, how likely 4:1, how likely 2:3
 for match in raw:
     if sum(match[:3]) == 0 or sum(match[:3]) == 3: three += 1
     elif sum(match[:4]) == 1 or sum(match[:4]) == 3: four += 1
